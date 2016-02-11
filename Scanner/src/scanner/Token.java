63e053b2b98e40b2db9d467a85dc5fa8bc44118d
lammyson <rlamsen@cedarville.edu>
2016-02-10 22:32:08 -0500

    Added TokenType enum to Token
    Updated .gitignore

--- Scanner/src/scanner/Token.java
+++ Scanner/src/scanner/Token.java
@@ -10,6 +10,39 @@ package scanner;
  * @author Ryan
  */
 public class Token {
+    
+    public enum TokenType {
+        ELSE_TOKEN,
+        IF_TOKEN,
+        INT_TOKEN,
+        RET_TOKEN,
+        VOID_TOKEN,
+        WHILE_TOKEN,
+        ADD_TOKEN,
+        SUB_TOKEN,
+        MUL_TOKEN,
+        DIV_TOKEN,
+        LESS_TOKEN,
+        LESS_EQ_TOKEN,
+        GREAT_TOKEN,
+        GREAT_EQ_TOKEN,
+        EQUAL_TOKEN,
+        NOT_EQ_TOKEN,
+        ASSIGN_TOKEN,
+        SEMI_TOKEN,
+        LPAREN_TOKEN,
+        RPAREN_TOKEN,
+        LSBRACK_TOKEN,
+        RSBRACK_TOKEN,
+        LCBRACK_TOKEN,
+        RCBRACK_TOKEN,
+        BEGIN_COMMENT_TOKEN,
+        END_COMMENT_TOKEN,
+        ID_TOKEN,
+        NUM_TOKEN,
+        LETTER_TOKEN,
+        DIGIT_TOKEN
+    }
 
     private TokenType tokenType;
     private Object tokenData;
