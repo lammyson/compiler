dfec9baaf1269cb28200417760a2c7e7550d3d9e
Andrew Lockridge <andrewlockridge@cedarville.edu>
2016-02-11 00:17:36 -0500

    Started working on scanToken function

--- Scanner/src/scanner/CMinusScanner.java
+++ Scanner/src/scanner/CMinusScanner.java
@@ -6,6 +6,7 @@
 package scanner;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import scanner.Token.TokenType;
 
 /**
@@ -52,7 +53,79 @@ public class CMinusScanner implements Scanner{
         int tokenStringIndex = 0;
         TokenType currentToken = null;
         StateType state = StateType.START;
-        int save;
+        boolean save;
+        while (state != StateType.DONE) {
+            try {
+                char c = (char)inFile.read();
+                save = true;
+                switch (state) {
+                    case START:
+                        if (Character.isDigit(c)) {
+                            state = StateType.INNUM;
+                        } else if (Character.isAlphabetic((int)c)) {
+                            state = StateType.INID;
+                        } else if (c == '=') {
+                            state = StateType.INASSIGN;
+                        } else if (c == '<') {
+                            state = StateType.INLESSTHAN;
+                        } else if (c == '>') {
+                            state = StateType.INGREATERTHAN;
+                        } else if ((c == ' ') || (c == '\t') || (c == '\n')) {
+                            save = false;
+                        } else if (c == '/') {
+                            state = StateType.INCOMMENT;
+                        } else {
+                            state = StateType.DONE;
+                            switch (c) {
+                                case EOF:
+                                    save = false;
+                                    currentToken = Token.TokenType.EOF_TOKEN;
+                                    break;
+                                case '+':
+                                    currentToken = Token.TokenType.ADD_TOKEN;
+                                    break;
+                                case '-':
+                                    currentToken = Token.TokenType.SUB_TOKEN;
+                                    break;
+                                case '*':
+                                    currentToken = Token.TokenType.MUL_TOKEN;
+                                    break;
+                                case '(':
+                                    currentToken = Token.TokenType.LPAREN_TOKEN;
+                                    break;
+                                case ')':
+                                    currentToken = Token.TokenType.RPAREN_TOKEN;
+                                    break;
+                                case '[':
+                                    currentToken = Token.TokenType.LSBRACK_TOKEN;
+                                    break;
+                                case ']':
+                                    currentToken = Token.TokenType.RSBRACK_TOKEN;
+                                    break;
+                                case '{':
+                                    currentToken = Token.TokenType.LCBRACK_TOKEN;
+                                    break;
+                                case '}':
+                                    currentToken = Token.TokenType.RCBRACK_TOKEN;
+                                    break;
+                                case ';':
+                                    currentToken = Token.TokenType.SEMI_TOKEN;
+                                    break;
+                            }
+                        }
+                    break;
+                    case INCOMMENT:
+                        if (c == '*') {
+                            
+                        }
+                        
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            
+        }
         
         return null;
     }
