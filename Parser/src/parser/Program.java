d69a0973daf0e63ff87ba81202dbc31ec37ff8ec
lammyson <rlamsen@cedarville.edu>
2016-02-29 23:54:54 -0500

    Added instance variables and parseProgram

--- Parser/src/parser/Program.java
+++ Parser/src/parser/Program.java
@@ -1,9 +1,15 @@
 package parser;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Ryan
  */
 public class Program {
+    private ArrayList<Declaration> program;
     
+    public Program parseProgram() {
+        return new Program();
+    }
 }
