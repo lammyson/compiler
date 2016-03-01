0456da228bbdd1d316f6a90100d0ddd5d2ba886f
lammyson <rlamsen@cedarville.edu>
2016-02-29 23:53:11 -0500

    Added OpType enum and instance variables

--- Parser/src/parser/BinaryExpression.java
+++ Parser/src/parser/BinaryExpression.java
@@ -5,5 +5,20 @@ package parser;
  * @author Ryan
  */
 public class BinaryExpression extends Expression {
+    public enum OpType {
+        PLUS,
+        MINUS,
+        MULT,
+        DIV,
+        LESS_THAN_EQ,
+        LESS_THAN,
+        GREATER_THAN,
+        GREATER_THAN_EQ,
+        EQUAL,
+        NOT_EQUAL
+    }
     
+    private Expression lhs;
+    private Expression rhs;
+    OpType  op;
 }
