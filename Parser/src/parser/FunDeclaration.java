0242bad31d05b21e1b92f62b4b297c6d89f59374
lammyson <rlamsen@cedarville.edu>
2016-02-29 23:59:33 -0500

    Added instance variables

--- Parser/src/parser/FunDeclaration.java
+++ Parser/src/parser/FunDeclaration.java
@@ -1,9 +1,14 @@
 package parser;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Ryan
  */
 public class FunDeclaration extends Declaration {
-    
+    private ArrayList<Param> paramList;
+    private Statement        compoundStatement;
+    private Statement        returnStatement;
+    private Expression       id;
 }
