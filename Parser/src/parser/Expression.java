1d7481bfaa7a7e724eeea23e81ff8d16cd5c2989
lammyson <rlamsen@cedarville.edu>
2016-03-01 22:05:24 -0500

    Added parseFactor

--- Parser/src/parser/Expression.java
+++ Parser/src/parser/Expression.java
@@ -1,5 +1,8 @@
 package parser;
 
+import scanner.Token;
+import scanner.CMinusScanner;
+
 /**
  *
  * @author Ryan
@@ -12,4 +15,24 @@ public abstract class Expression {
     public Expression parseExpression() {
         return new BinaryExpression();
     }
+    
+    private Expression parseFactor () {
+        switch (viewNextToken()) {
+            case Token.TokenType.LPAREN_TOKEN:
+                advanceToken();
+                Expression returnExpr = parseExpression ();
+                matchToken(Token.TokenType.RPAREN_TOKEN);
+                return returnExpr;
+            case Token.TokenType.ID_TOKEN:
+                Token oldToken = advanceToken();
+                return IdExpression(oldToken);
+            case Token.TokenType.NUM_TOKEN:
+               Token oldToken = advanceToken();
+                return NumExpression(oldToken);
+            default:
+                //logParseError();
+                return null;
+        }
+    }
+
 }
