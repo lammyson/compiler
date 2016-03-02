dae16e7534d8a9ef33aff9e61f68cc7a0569720a
lammyson <rlamsen@cedarville.edu>
2016-03-01 21:04:00 -0500

    Added parseExpression

--- Parser/src/parser/Expression.java
+++ Parser/src/parser/Expression.java
@@ -5,5 +5,11 @@ package parser;
  * @author Ryan
  */
 public abstract class Expression {
+    public Expression() {
+        
+    }
     
+    public Expression parseExpression() {
+        return new BinaryExpression();
+    }
 }
