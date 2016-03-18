e9e01007992ec8228b1a5bb582c172893660c2a3
lammyson <rlamsen@cedarville.edu>
2016-03-17 23:03:25 -0400

    Added variables, constructor, and printMe

--- Parser/src/parser/NumExpression.java
+++ Parser/src/parser/NumExpression.java
@@ -1,9 +1,27 @@
 package parser;
 
 /**
- *
+ * This class is for the NUM terminal in the C Minus language
  * @author Ryan
  */
 public class NumExpression extends Expression {
+    /**
+     * This variable holds the string name of the ID
+     */
+    private int num;
     
+    /**
+     * Constructor
+     * @param n
+     */
+    public NumExpression(int n) {
+        num = n;
+    }
+    
+    /**
+     * This method will print the attributes of IdExpression
+     */
+    private void printMe() {
+        
+    }
 }
