a076e76aa6e70857d23440d420a2281531c20ade
lammyson <rlamsen@cedarville.edu>
2016-03-17 23:06:01 -0400

    Added comments

--- Parser/src/parser/Expression.java
+++ Parser/src/parser/Expression.java
@@ -1,38 +1,14 @@
 package parser;
 
-import scanner.Token;
-import scanner.CMinusScanner;
-
 /**
- *
+ * This acts as a parent class for:
+ *      AssignExpression
+ *      BinaryExpression
+ *      IdExpression
+ *      NumExpression
+ *      VarCallExpression
  * @author Ryan
  */
 public abstract class Expression {
-    public Expression() {
-        
-    }
-    
-    public Expression parseExpression() {
-        return new BinaryExpression();
-    }
-    
-    private Expression parseFactor () {
-        switch (viewNextToken()) {
-            case Token.TokenType.LPAREN_TOKEN:
-                advanceToken();
-                Expression returnExpr = parseExpression ();
-                matchToken(Token.TokenType.RPAREN_TOKEN);
-                return returnExpr;
-            case Token.TokenType.ID_TOKEN:
-                Token oldToken = advanceToken();
-                return IdExpression(oldToken);
-            case Token.TokenType.NUM_TOKEN:
-               Token oldToken = advanceToken();
-                return NumExpression(oldToken);
-            default:
-                //logParseError();
-                return null;
-        }
-    }
 
 }
