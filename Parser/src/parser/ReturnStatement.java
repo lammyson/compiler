84a4ed4dc3700728e2755dac9bf4d1f556bd72d3
lammyson <rlamsen@cedarville.edu>
2016-03-17 23:35:30 -0400

    Added variables, constructor, and printMe

--- Parser/src/parser/ReturnStatement.java
+++ Parser/src/parser/ReturnStatement.java
@@ -1,9 +1,28 @@
 package parser;
 
 /**
- *
+ * This class defines a return-stmt in the C Minus language
  * @author Ryan
  */
 public class ReturnStatement extends Statement {
     
+    /**
+     * This variable holds an optional expression in the return statement
+     */
+    private Expression expression;
+    
+    /**
+     * Constructor
+     * @param expr 
+     */
+    public ReturnStatement(Expression expr) {
+        expression = expr;
+    }
+    
+    /**
+     * This method will print the attributes of ReturnStatement
+     */
+    private void printMe() {
+        
+    }
 }
