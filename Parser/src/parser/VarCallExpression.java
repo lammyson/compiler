f622fd6eac59c27791ff6f8382eece6a86907e64
lammyson <rlamsen@cedarville.edu>
2016-03-17 23:49:45 -0400

    Added variables, constructor, and printMe

--- Parser/src/parser/VarCallExpression.java
+++ Parser/src/parser/VarCallExpression.java
@@ -1,9 +1,52 @@
 package parser;
 
+import java.util.ArrayList;
+
 /**
- *
+ * This class combines the var and call nonterminals in the C Minus language
  * @author Ryan
  */
 public class VarCallExpression extends Expression {
     
+    /**
+     * This variable holds the ID of the var or call
+     */
+    private IdExpression          identifier;
+    
+    /**
+     * This variable holds an expression for an array var. Can be null.
+     */
+    private Expression            expression;
+    
+    /**
+     * This variable holds the args for a call. Can be null.
+     */
+    private ArrayList<Expression> argsList;
+    
+    /**
+     * This variable tells if it is a var or a call
+     * 0 for var. 1 for call.
+     */
+    private int                   varOrCall;
+    
+    /**
+     * Constructor
+     * @param id
+     * @param expr
+     * @param args 
+     */
+    public VarCallExpression(IdExpression id, Expression expr, 
+                             ArrayList<Expression> args, int varcall) {
+        identifier = id;
+        expression = expr;
+        argsList   = args;
+        varOrCall  = varcall;
+    }
+    
+    /**
+     * This method will print the attributes of VarCallExpression
+     */
+    private void printMe() {
+        
+    }
 }
