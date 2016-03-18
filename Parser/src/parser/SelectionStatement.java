9b6c9c0277038fc0becf1b68cfe4bccd38984f8d
lammyson <rlamsen@cedarville.edu>
2016-03-17 23:39:36 -0400

    Added variables, constructor, and printMe

--- Parser/src/parser/SelectionStatement.java
+++ Parser/src/parser/SelectionStatement.java
@@ -1,9 +1,41 @@
 package parser;
 
 /**
- *
+ * This class defines a selection-stmt in the C Minus language
+ * It is an if statement with an optional else statement
  * @author Ryan
  */
 public class SelectionStatement extends Statement {
+       
+    /**
+     * This variable holds the condition of the if statement
+     */
+    private Expression expression;
     
+    /**
+     * This variable holds the statement attached to the if portion
+     */
+    private Statement  ifStatement;
+    
+    /**
+     * This variable holds the statement attached to the else portion
+     */
+    private Statement  elseStatement;
+    
+    /**
+     * Constructor
+     * @param expr 
+     */
+    public SelectionStatement(Expression expr, Statement ifstmt, Statement elsestmt) {
+        expression    = expr;
+        ifStatement   = ifstmt;
+        elseStatement = elsestmt;
+    }
+    
+    /**
+     * This method will print the attributes of SelectionStatement
+     */
+    private void printMe() {
+        
+    } 
 }
