c6d88773190c79bcbc49b4266bfcbe5a75e8e78c
lammyson <rlamsen@cedarville.edu>
2016-03-17 23:54:33 -0400

    Added variables, constructor, and printMe

--- Parser/src/parser/AssignExpression.java
+++ Parser/src/parser/AssignExpression.java
@@ -1,10 +1,35 @@
 package parser;
 
 /**
- * This class defines 
+ * This class defines an Assign Expression in the C Minus language
+ * It is of the form var = expression;
  * @author Ryan
  */
 public class AssignExpression extends Expression {
-    private Expression id;
-    private Expression expr;
+    /**
+     * This holds the var that will take the expression value
+     */
+    private VarCallExpression var;
+    
+    /**
+     * This holds the expression that will be assigned into the var
+     */
+    private Expression expression;
+    
+    /**
+     * Construction
+     * @param v
+     * @param expr 
+     */
+    public AssignExpression(VarCallExpression v, Expression expr) {
+        var        = v;
+        expression = expr;
+    }
+    
+    /**
+     * This method prints the attributes of AssignExpression
+     */
+    private void printMe() {
+        
+    }
 }
