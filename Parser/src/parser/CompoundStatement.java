4c797e15619a4947ebc0d1409dca3ac26eaece54
lammyson <rlamsen@cedarville.edu>
2016-03-17 23:16:40 -0400

    Added variables, constructor, and printMe

--- Parser/src/parser/CompoundStatement.java
+++ Parser/src/parser/CompoundStatement.java
@@ -1,9 +1,38 @@
 package parser;
 
+import java.util.ArrayList;
+
 /**
- *
+ * This class defines a compound-stmt in the C Minus language
  * @author Ryan
  */
 public class CompoundStatement extends Statement {
     
+    /**
+     * This variable holds a list of variable declarations. Can be null
+     */
+    private ArrayList<VarDeclaration> varDeclarations;
+    
+    /**
+     * This variable holds a list of statements. Can be null
+     */
+    private ArrayList<Statement>      statements;
+    
+    /**
+     * Constructor
+     * @param decls
+     * @param stmts 
+     */
+    public CompoundStatement(ArrayList<VarDeclaration> decls,
+                             ArrayList<Statement> stmts) {
+        varDeclarations = decls;
+        statements      = stmts;
+    }
+    
+    /**
+     * This method will print the attributes of CompoundStatement
+     */
+    private void printMe() {
+        
+    }
 }
