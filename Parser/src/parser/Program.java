3c88c5c0cfe46ed7a9e19634bdec23408e4a2787
lammyson <rlamsen@cedarville.edu>
2016-03-17 21:44:25 -0400

    Deleted parseProgram

--- Parser/src/parser/Program.java
+++ Parser/src/parser/Program.java
@@ -12,16 +12,4 @@ public class Program {
     public Program(ArrayList<Declaration> declList) {
         program = declList;
     }
-    
-    public Program parseProgram() {
-        ArrayList<Declaration> declList = new ArrayList();
-        
-        declList.add(parseDeclaration());
-        
-        while() {
-            declList.add(parseDeclaration());
-        }
-        
-        return new Program(declList);
-    }
 }
