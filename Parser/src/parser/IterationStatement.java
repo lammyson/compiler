8aca29710ae5d0e8ce55cc2c700fe1f8e6d87aaa
lammyson <rlamsen@cedarville.edu>
2016-03-17 23:32:25 -0400

    Added variables, constructor, and printMe

--- Parser/src/parser/IterationStatement.java
+++ Parser/src/parser/IterationStatement.java
@@ -1,9 +1,36 @@
 package parser;
 
 /**
- *
+ * This class defines an iteration-stmt of the C Minus language
+ * It is a while loop
  * @author Ryan
  */
 public class IterationStatement extends Statement {
     
+    /**
+     * This variable holds the condition for the while loop
+     */
+    private Expression expression;
+    
+    /**
+     * This holds the definition of the while loop
+     */
+    private Statement  statement;
+    
+    /**
+     * Constructor
+     * @param expr
+     * @param stmt 
+     */
+    public IterationStatement(Expression expr, Statement stmt) {
+        expression = expr;
+        statement  = stmt;
+    }
+    
+    /**
+     * This method will print the attributes of IterationStatement
+     */
+    private void printMe() {
+        
+    }
 }
