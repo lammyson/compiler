94a37377d2d5bafa53fb4890e6875319ef9b1a82
lammyson <rlamsen@cedarville.edu>
2016-03-17 23:08:12 -0400

    Added comments and printMe

--- Parser/src/parser/Program.java
+++ Parser/src/parser/Program.java
@@ -3,13 +3,27 @@ package parser;
 import java.util.ArrayList;
 
 /**
- *
+ * This class is the root for an abstract syntax tree
  * @author Ryan
  */
 public class Program {
+    /**
+     * This variable holds the abstract syntax tree for a program
+     */
     private ArrayList<Declaration> program;
     
+    /**
+     * Constructor
+     * @param declList 
+     */
     public Program(ArrayList<Declaration> declList) {
         program = declList;
+    }
+    
+    /**
+     * This method will print the attributes of Program
+     */
+    private void printMe() {
+        
     }
 }
