71f2a35d40978d8c94f9441e079c512df45def37
lammyson <rlamsen@cedarville.edu>
2016-03-22 15:43:53 -0400

    Wrote printMe

--- Parser/src/parser/ReturnStatement.java
+++ Parser/src/parser/ReturnStatement.java
@@ -1,6 +1,7 @@
 package parser;
 
 import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * This class defines a return-stmt in the C Minus language
@@ -26,6 +27,29 @@ public class ReturnStatement extends Statement {
      * @param out
      */
     public void printMe(FileWriter out, int indent) {
-        
+        try {
+            for (int i = 0; i < indent; i++) {
+                out.write("  ");
+            }
+            out.write("IterationStatement" + '\n');
+
+            if (expression == null) {
+                out.write("No return expression");
+            } else if (expression instanceof AssignExpression) {
+                ((AssignExpression) expression).printMe(out, indent + 1);
+            } else if (expression instanceof BinaryExpression) {
+                ((BinaryExpression) expression).printMe(out, indent + 1);
+            } else if (expression instanceof CallExpression) {
+                ((CallExpression) expression).printMe(out, indent + 1);
+            } else if (expression instanceof NumExpression) {
+                ((NumExpression) expression).printMe(out, indent + 1);
+            } else if (expression instanceof VarExpression) {
+                ((VarExpression) expression).printMe(out, indent + 1);
+            }
+            out.write('\n');
+        }
+        catch (IOException e) {
+            System.out.println("Error writing to file in CompoundStatement");
+        }
     }
 }
