23c50bee0df66f0f16592b18ce32caffcf8afc2c
lammyson <rlamsen@cedarville.edu>
2016-03-21 23:13:04 -0400

    Added comments

--- Parser/src/parser/CallExpression.java
+++ Parser/src/parser/CallExpression.java
@@ -1,31 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package parser;
 
 import java.io.FileWriter;
 import java.util.ArrayList;
 
 /**
- *
+ * This class defines the call part of a var call which is a function call
  * @author Andrew
  */
 public class CallExpression extends Expression {
     
-    String id;
+    /**
+     * This variable holds the id for the call
+     */
+    private String                id;
     
-    ArrayList<Expression> argList;
+    /**
+     * This variable holds the args-list inside the call
+     */
+    private ArrayList<Expression> argList;
     
+    /**
+     * Constructor
+     * @param id
+     * @param argList 
+     */
     public CallExpression(String id, ArrayList<Expression> argList) {
         this.id = id;
         this.argList = argList;
     }
     
+    /**
+     * This method prints out the attributes of a function call
+     * @param out 
+     */
     public void printMe(FileWriter out) {
         
     }
-    
 }
