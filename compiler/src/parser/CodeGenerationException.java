c0e7145e4e7a60e0c3377c49c4af3d3ed6b69205
lammyson <rlamsen@cedarville.edu>
2016-04-11 20:02:39 -0400

    Added this class to fix error in compiler.Operand

---
+++ compiler/src/parser/CodeGenerationException.java
@@ -0,0 +1,11 @@
+package parser;
+
+/**
+ *
+ * @author Ryan
+ */
+public class CodeGenerationException extends RuntimeException {
+    public CodeGenerationException(String errorMsg) {
+        super(errorMsg);
+    }
+}
