e7c6052185e95652069d0e4284dd3039baab6040
lammyson <rlamsen@cedarville.edu>
2016-04-26 23:30:46 -0400

    Implemented genLLCode method

--- compiler/src/parser/ReturnStatement.java
+++ compiler/src/parser/ReturnStatement.java
@@ -2,6 +2,11 @@ package parser;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import lowlevel.Operation;
+import lowlevel.Operation.OperationType;
+import lowlevel.BasicBlock;
+import lowlevel.Function;
+import lowlevel.Operand;
 
 /**
  * This class defines a return-stmt in the C Minus language
@@ -40,5 +45,23 @@ public class ReturnStatement extends Statement {
         catch (IOException e) {
             System.out.println("Error writing to file in ReturnStatement");
         }
+    }
+    
+    
+    public void genLLCode(Function func) {
+        BasicBlock currBlock = func.getCurrBlock();
+        BasicBlock returnBlock = func.getReturnBlock();
+        if (expression != null) {
+            expression.genLLCode(currBlock);
+            Operation newOper = 
+                    new Operation(Operation.OperationType.RETURN, currBlock);
+            Operand src = new Operand(Operand.OperandType.MACRO,"RetReg");
+            newOper.setSrcOperand(0, src);
+            currBlock.appendOper(newOper);
+        }
+        Operation newOper = new Operation(OperationType.JMP, currBlock);
+        Operand src = new Operand(Operand.OperandType.BLOCK, returnBlock);
+        newOper.setSrcOperand(0, src);
+        currBlock.appendOper(newOper);
     }
 }
